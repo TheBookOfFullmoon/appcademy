2cd5e878878fce36aaeb65a68cb2d59bbb2a84d2
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-05 03:16:02 +0800

    Adding relationship function to Student and User model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'birthday',
+      'birth_place',
+      'address',
+      'gender',
+      'phone',
+      'major_id',
+      'user_id',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
