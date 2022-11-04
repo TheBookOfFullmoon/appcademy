079e859a11189ccfede2ca7f5618a2f3fec0489f
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-05 03:16:50 +0800

    Creating CRUD function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
-    //
+    public function index(){
+        if (Auth::user()->role = "admin"){
+            return "hai admin";
+        }
+
+        return "hai student";
+    }
+
+    public function create(){
+        return "create page";
+    }
+
+    public function store(StoreStudentRequest $request){
+        Student::create($request->post());
+
+        return redirect()->route('admin.students.index')
+            ->with('success', 'Successfully created new student');
+    }
+
+    public function edit(Student $student){
+        return $student;
+    }
+
+    public function update(UpdateStudentRequest $request, Student $student){
+        $student->update($request->post());
+
+        return redirect()->route('admin.students.index')
+            ->with('success', "Successfully updated student");
+    }
+
+    public function destroy(Student $student){
+        $student->delete();
+
+        return redirect()->route("admin.students.index")
+            ->with('success', "Successfully deleted student");
+    }
 }
