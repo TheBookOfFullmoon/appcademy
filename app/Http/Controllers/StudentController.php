4bed6d2bf6de57c35662df107636be0649f3bd99
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-06 20:12:46 +0800

    Correcting controller to generate users everytime student data is saved or updated

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateStudentRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,8 +25,23 @@ class StudentController extends Controller
         return "create page";
     }
 
-    public function store(StoreStudentRequest $request){
-        Student::create($request->post());
+    public function store(StoreStudentRequest $studentRequest, StoreUserRequest $userRequest){
+        $user = User::create([
+            'email' => $userRequest->post('email'),
+            'password' => 'password',
+            'role' => $userRequest->post('role')
+        ]);
+
+        Student::create([
+            'name' => $studentRequest->post('name'),
+            'birthday' => $studentRequest->post('birthday'),
+            'birth_place' => $studentRequest->post('birth_place'),
+            'address' => $studentRequest->post('address'),
+            'gender' => $studentRequest->post('gender'),
+            'phone' => $studentRequest->post('phone'),
+            'major_id' => $studentRequest->post('major_id'),
+            'user_id'=> $user->id
+        ]);
 
         return redirect()->route('admin.students.index')
             ->with('success', 'Successfully created new student');
@@ -33,8 +51,24 @@ class StudentController extends Controller
         return $student;
     }
 
-    public function update(UpdateStudentRequest $request, Student $student){
-        $student->update($request->post());
+    public function update(UpdateStudentRequest $studentRequest, Student $student, UpdateUserRequest $userRequest){
+        $user = User::find($student->user->id);
+
+        $user->update([
+            'email' => $userRequest->post('email'),
+            'password' => $userRequest->post('password'),
+            'role' => $userRequest->post('role')
+        ]);
+
+        $student->update([
+            'name' => $studentRequest->post('name'),
+            'birthday' => $studentRequest->post('birthday'),
+            'birth_place' => $studentRequest->post('birth_place'),
+            'address' => $studentRequest->post('address'),
+            'gender' => $studentRequest->post('gender'),
+            'phone' => $studentRequest->post('phone'),
+            'major_id' => $studentRequest->post('major_id'),
+        ]);
 
         return redirect()->route('admin.students.index')
             ->with('success', "Successfully updated student");
@@ -42,6 +76,10 @@ class StudentController extends Controller
 
     public function destroy(Student $student){
         $student->delete();
+
+        $user = User::find($student->user->id);
+
+        $user->delete();
 
         return redirect()->route("admin.students.index")
             ->with('success', "Successfully deleted student");
