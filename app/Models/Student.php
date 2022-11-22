affc6fc7cec5d10378b4c1a0f97c5afa1dc63de8
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-23 01:49:22 +0800

    Adding major relation function and add accessor and mutator for birthday attribute of student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
@@ -23,5 +26,17 @@ class Student extends Model
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function major(): BelongsTo
+    {
+        return $this->belongsTo(Major::class);
+    }
+
+    protected function birthday(): Attribute{
+        return Attribute::make(
+          get: fn($value) => Carbon::createFromFormat('d/m/Y', $value)->format('d-m-Y'),
+          set: fn($value) => Carbon::parse($value)->format('d/m/Y')
+        );
     }
 }
