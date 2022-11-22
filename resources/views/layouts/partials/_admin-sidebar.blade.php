78b74908f860dc9e0319141936b862210a97d54c
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-23 01:39:35 +0800

    Updating the remainder links of admin menu

--- resources/views/layouts/partials/_admin-sidebar.blade.php
+++ resources/views/layouts/partials/_admin-sidebar.blade.php
@@ -1,4 +1,5 @@
 <li class="nav-item"><a class="nav-link {{(request()->is('admin')) ? 'active' : ''}}" href="{{route('admin.index')}}"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
 <li class="nav-item"><a class="nav-link {{(request()->is('admin/majors*')) ? 'active' : ''}}" href="{{route('admin.majors.index')}}"><i class="fa fa-university"></i><span>Major</span></a></li>
-<li class="nav-item"><a class="nav-link" href="profile.html"><i class="fas fa-user"></i><span>Lecturer &amp; Student</span></a></li>
-<li class="nav-item"><a class="nav-link" href="login.html"><i class="fa fa-book"></i><span>Subject</span></a></li>
+<li class="nav-item"><a class="nav-link {{(request()->is('admin/lecturers*')) ? 'active' : ''}}" href="{{route('admin.lecturers.index')}}"><i class="fas fa-user"></i><span>Lecturer</span></a></li>
+<li class="nav-item"><a class="nav-link {{(request()->is('admin/student*')) ? 'active' : ''}}" href="{{route('admin.students.index')}}"><i class="fas fa-user"></i><span>Student</span></a></li>
+<li class="nav-item"><a class="nav-link {{(request()->is('admin/subjects*')) ? 'active' : ''}}" href="{{route('admin.subjects.index')}}"><i class="fa fa-book"></i><span>Subject</span></a></li>
