fb2616071643099244041adc4b8cdd893bf9ad8d
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-23 01:38:31 +0800

    Changing data of birthday to string on students table migration

--- database/migrations/2022_11_04_181700_create_students_table.php
+++ database/migrations/2022_11_04_181700_create_students_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->date('birthday');
+            $table->string('birthday');
             $table->string('birth_place');
             $table->string('gender');
             $table->string('address');
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('student');
     }
 };
