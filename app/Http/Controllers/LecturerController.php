8f6e71a8769498b838ff448efd3bff6b0257d349
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-26 00:46:46 +0800

    Updating all function to return to their corresponding views and adding search function to the controller

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -7,29 +7,33 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateLecturerRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\Lecturer;
+use App\Models\Major;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class LecturerController extends Controller
 {
     public function index(){
         if (Auth::user()->role == 'admin'){
-            return "hai admin";
+            $lecturers = Lecturer::with('user')->paginate(5);
+
+            return view('admin.lecturer.index', compact('lecturers'));
         }
 
         return "hai lecturer";
     }
 
     public function create(){
-        return "create page";
+        return view('admin.lecturer.create');
     }
 
     public function store(StoreLecturerRequest $lecturerRequest, StoreUserRequest $userRequest){
         $user = User::create([
             'email' => $userRequest->post('email'),
             'password' => 'password',
-            'role' => $userRequest->post('role')
+            'role' => 'lecturer'
         ]);
 
         Lecturer::create([
@@ -47,7 +51,10 @@ class LecturerController extends Controller
     }
 
     public function edit(Lecturer $lecturer){
-        return $lecturer;
+        $birthday = Carbon::parse($lecturer->birthday)->format('Y-m-d');
+//        dd($birthday);
+
+        return view('admin.lecturer.edit', compact('lecturer', 'birthday'));
     }
 
     public function update(UpdateLecturerRequest $lecturerRequest, Lecturer $lecturer, UpdateUserRequest $userRequest){
@@ -55,8 +62,8 @@ class LecturerController extends Controller
 
         $user->update([
             'email' => $userRequest->post('email'),
-            'password' => $userRequest->post('password'),
-            'role' => $userRequest->post('role')
+            'password' => $user->password,
+            'role' => 'lecturer'
         ]);
 
         $lecturer->update([
@@ -78,13 +85,28 @@ class LecturerController extends Controller
     }
 
     public function destroy(Lecturer $lecturer){
-        $lecturer->delete();
-
         $user = User::find($lecturer->user->id);
-
         $user->delete();
+
+        $lecturer->delete();
 
         return redirect()->route("admin.lecturers.index")
             ->with('success', "Successfully deleted lecturer");
+    }
+
+    public function search(Request $request){
+        $lecturers = Lecturer::with('user')
+            ->where('name', 'LIKE', '%'.$request->get('keyword').'%')
+            ->orWhere('birth_place', 'LIKE', '%'.$request->get('keyword').'%')
+            ->orWhere('birthday', 'LIKE', '%'.$request->get('keyword').'%')
+            ->orWhere('address', 'LIKE', '%'.$request->get('keyword').'%')
+            ->orWhere('gender', 'LIKE', '%'.$request->get('keyword').'%')
+            ->orWhere('phone', 'LIKE', '%'.$request->get('keyword').'%')
+            ->orWhereHas('user', function($q) use($request){
+                return $q->where('email', 'LIKE', '%'.$request->get('keyword'));
+            })
+            ->paginate(5);
+
+        return view('admin.lecturer.index', compact('lecturers'));
     }
 }
