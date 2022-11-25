a57f373284aa274f4098d5a4602828b23a3383cb
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-26 00:47:09 +0800

    Adding accessor and mutator for birthday attribute

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,5 +19,12 @@ class Lecturer extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    protected function birthday(): Attribute{
+        return Attribute::make(
+            get: fn($value) => Carbon::createFromFormat('d/m/Y', $value)->format('d-m-Y'),
+            set: fn($value) => Carbon::parse($value)->format('d/m/Y')
+        );
     }
 }
