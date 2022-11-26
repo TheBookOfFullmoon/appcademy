e881556008487c159f5af5c51c3cc92a52ba0c43
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-26 12:17:28 +0800

    Refactoring the controller to direct to the proper views and adding the search function

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,33 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreScheduleRequest;
 use App\Http\Requests\StoreSubjectRequest;
+use App\Http\Requests\UpdateScheduleRequest;
 use App\Http\Requests\UpdateSubjectRequest;
+use App\Models\Lecturer;
+use App\Models\Schedule;
 use App\Models\Subject;
+use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
     public function index(){
-        return "subject page";
+        $subjects = Subject::with('lecturer', 'schedule')
+            ->paginate(5);
+
+        return view('admin.subject.index', compact('subjects'));
     }
 
     public function create(){
-        return "subject create page";
+        $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+        $lecturers = Lecturer::all();
+
+        return view('admin.subject.create', compact('days', 'lecturers'));
     }
 
-    public function store(StoreSubjectRequest $request){
-        Subject::create($request->post());
+    public function store(StoreSubjectRequest $subjectRequest, StoreScheduleRequest $scheduleRequest){
+        $subject = Subject::create([
+            'name' => $subjectRequest->post('name'),
+            'sks' => $subjectRequest->post('sks'),
+            'lecturer_id' => $subjectRequest->post('lecturer_id')
+        ]);
+
+        Schedule::create([
+            'day_name' => $scheduleRequest->post('day_name'),
+            'room' => $scheduleRequest->post('room'),
+            'subject_id' => $subject->id
+        ]);
 
         return redirect()->route('admin.subjects.index')
             ->with('success', "Successfully created a new subject");
     }
 
     public function edit(Subject $subject){
-        return $subject;
+        $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+        $lecturers = Lecturer::all();
+
+        return view('admin.subject.edit', compact('days', 'lecturers', 'subject'));
     }
 
-    public function update(UpdateSubjectRequest $request, Subject $subject){
-        $subject->update($request->post());
+    public function update(UpdateSubjectRequest $subjectRequest, Subject $subject, UpdateScheduleRequest $scheduleRequest){
+        $subject->update([
+            'name' => $subjectRequest->name,
+            'sks' => $subjectRequest->sks,
+            'lecturer_id' => $subjectRequest->lecturer_id
+        ]);
+
+        $schedule = Schedule::where('subject_id', '=', $subject->id);
+
+        $schedule->update([
+            'day_name' => $scheduleRequest->day_name,
+            'room' => $scheduleRequest->room
+        ]);
 
         return redirect()->route("admin.subjects.index")
             ->with('success', "Successfully updated subject");
@@ -39,5 +76,13 @@ class SubjectController extends Controller
 
         return redirect()->route("admin.subjects.index")
             ->with('success', "Successfully deleted subject");
+    }
+
+    public function search(Request $request){
+        $subjects = Subject::with('lecturer', 'schedule')
+            ->search($request->get('keyword'))
+            ->paginate(5);
+
+        return view('admin.subject.index', compact('subjects'));
     }
 }
