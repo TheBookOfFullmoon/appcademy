d98a7c1ce7df23ae0b08255ab0bbefe7fc0777f8
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-28 06:24:36 +0800

    Updating StudentSubjectTest to test if assigned and unassigned page retrieve the correct data

--- tests/Feature/StudentSubjectTest.php
+++ tests/Feature/StudentSubjectTest.php
@@ -2,6 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\Lecturer;
+use App\Models\Major;
+use App\Models\Student;
+use App\Models\Subject;
+use App\Models\User;
+use Database\Factories\StudentSubjectFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Schema;
@@ -22,5 +28,51 @@ class StudentSubjectTest extends TestCase
                 'student_id', 'subject_id', 'grade'
             ]), 1
         );
+    }
+
+    public function test_visit_assigned_and_unassigned_student_page()
+    {
+        $user = User::factory()->create(['role' => 'admin']);
+
+        $major = Major::factory()->create();
+
+        $userStudent = User::factory()->create(['role' => 'student']);
+        $student = Student::factory()->create([
+           'user_id' => $userStudent->id,
+           'major_id' => $major->id
+        ]);
+
+        $userStudent2 = User::factory()->create(['role' => 'student']);
+        $student2 = Student::factory()->create([
+            'user_id' => $userStudent->id,
+            'major_id' => $major->id
+        ]);
+
+        $userLecturer = User::factory()->create(['role' => 'lecturer']);
+        $lecturer = Lecturer::factory()->create([
+            'user_id' => $userLecturer->id,
+        ]);
+
+        $subject = Subject::factory()->create([
+            'lecturer_id' => $lecturer->id
+        ]);
+        $subject2 = Subject::factory()->create([
+            'lecturer_id' => $lecturer->id
+        ]);
+
+        $student->subjects()->attach($subject->id);
+        $student2->subjects()->attach($subject2->id);
+
+        $this->assertDatabaseCount('student_subject', 2);
+
+        $this->actingAs($user)->get(route('admin.subjects.assigned', $subject->id))
+            ->assertStatus(200)
+            ->assertSeeText($student->name)
+            ->assertDontSeeText($student2->name);
+
+        $this->actingAs($user)->get(route('admin.subjects.unassigned', $subject->id))
+            ->assertStatus(200)
+            ->assertSeeText($student2->name)
+            ->assertDontSeeText($student->name);
     }
 }
