007c4850aa324bdfdc223de8957073f6d47a613b
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-28 23:03:23 +0800

    Updating the action button to unassign the student from the subject. Adding SweetAlert2 Modal to prompt before unassigning and Toast to show the success message

--- resources/views/admin/subject/assigned.blade.php
+++ resources/views/admin/subject/assigned.blade.php
@@ -2,6 +2,12 @@
 
 @section('page_title', "Students Assigned to $subject->name")
 
+@section('alert')
+    @if (session('success'))
+        <div id="success-message" data-message="{{session('success')}}"></div>
+    @endif
+@endsection
+
 @section('content')
     <div class="card shadow">
         <div class="card-header py-3">
@@ -41,14 +47,10 @@
                         <tr>
                             <td>{{$student->name}}</td>
                             <td>
-                                <form action="{{route('admin.subjects.destroy', $subject->id)}}" method="post">
-                                    <div class="btn-group" role="group">
-                                        <a class="btn btn-success btn-sm" href="{{route('admin.subjects.edit', $subject->id)}}" style="color: rgb(255,255,255);"><i class="fa fa-pencil"></i></a>
-
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-danger btn-sm delete" data-name="{{$subject->name}}"><i class="fa fa-trash"></i></button>
-                                    </div>
+                                <form action="{{route('admin.subjects.unassign', ['subject' => $subject->id, 'student' => $student->id])}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm unassign" data-student="{{$student->name}}" data-subject="{{$subject->name}}"><i class="fa fa-times"></i></button>
                                 </form>
                             </td>
                         </tr>
@@ -64,4 +66,58 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('js')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        let elements = document.getElementsByClassName("unassign");
+
+        let unassignModal = function(e) {
+            e.preventDefault();
+            let studentName = this.getAttribute("data-student");
+            let subjectName = this.getAttribute("data-subject");
+            let form = this.closest("form");
+
+            Swal.fire({
+                title: `Are you sure want to unassign ${studentName} from ${subjectName}?`,
+                text: ``,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, unassign it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                }
+            })
+        }
+
+        Array.from(elements).forEach(function(element) {
+            element.addEventListener('click', unassignModal);
+        });
+
+        let success = document.getElementById("success-message");
+
+        if (success != null){
+            let message = success.getAttribute("data-message");
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: message
+            })
+        }
+    </script>
 @endsection
