4ebf20cff3a81374b538c3d35e190d7e3d983e3e
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-11-30 21:34:43 +0800

    Updating Major and Subject Factory to use fake word instead of name for its name attribute

--- database/factories/MajorFactory.php
+++ database/factories/MajorFactory.php
@@ -17,7 +17,7 @@ class MajorFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name
+            'name' => fake()->word
         ];
     }
 }
--- database/factories/SubjectFactory.php
+++ database/factories/SubjectFactory.php
@@ -17,7 +17,7 @@ class SubjectFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name,
+            'name' => fake()->word,
             'sks' => rand(1, 3)
         ];
     }
